7e11153cacbc06e630ea1208f63a9d33e467e5e1
Erich Gaul <erichgaul@gmail.com>
2023-08-01 09:14:36 -0500

    Add JS functionality to operator buttons, equals button, and pos-neg button.  Ensure basic calculator functions are working properly

--- scripts.js
+++ scripts.js
@@ -1,39 +1,121 @@
-let firstNum
-let secondNum
-let operator
+let firstNum = "";
+let secondNum = "";
+let holder = ""
+let chosenOperator = ""
+let displayNow = ""
+let result = ""
+
 
 // Add //
-function add(firstNum, secondNum) {
-    return firstNum + secondNum;
+function add(x, y) {
+    return x + y;
 };
 
 // Subtract //
-function subtract(firstNum, secondNum) {
-    return firstNum - secondNum;
+function subtract(x, y) {
+    return x - y;
 }
 
 // Multiply //
-function multiply(firstNum, secondNum) {
-    return firstNum * secondNum;
+function multiply(x, y) {
+    return x * y;
 };
 
 // Divide //
-function divide(firstNum, secondNum) {
-    return firstNum / secondNum;
+function divide(x, y) {
+    return x / y;
 };
 
 // Operate //
 function operate(op, x, y) {
-    firstNum = x;
-    secondNum = y;
-    operator = op
+    let cleanX
+    let cleanY
+    if (x.indexOf(".") === -1 && y.indexOf(".") === -1) {
+        cleanX = parseInt(x);
+        cleanY = parseInt(y);
+    } else {
+        cleanX = parseFloat(x);
+        cleanY = parseFloat(y);
+    };
+
     if (op === "+") {
-        return add(firstNum, secondNum);
+        return add(cleanX, cleanY);
     } else if (op === "-") {
-        return subtract(x, y);
+        return subtract(cleanX, cleanY);
     } else if (op === "*") {
-        return multiply(x, y);
+        return multiply(cleanX, cleanY);
     } else if (op === "/") {
-        return divide(x, y);
+        return divide(cleanX, cleanY);
     };
 };
+
+// Display numbers when clicked //
+
+const numberButton = document.querySelectorAll('.number');
+numberButton.forEach(function (i) {
+    i.addEventListener('click', function() {
+        displayNumber(this);
+    });
+});
+
+function displayNumber(number) {
+    const num = number.textContent;
+    const display = document.querySelector('.numberDisplay');
+    currentDisplay = display.textContent;
+    display.textContent = currentDisplay + num;
+    displayNow = display.textContent;
+};
+
+// Enable clear button
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', function() {
+    const display = document.querySelector('.numberDisplay');
+    display.textContent = "";
+    firstNum = "";
+    secondNum = "";
+    chosenOperator = "";
+    displayNow = display.textContent;
+});
+
+// Enable equals button
+const equalsButton = document.querySelector('.equals');
+equalsButton.addEventListener('click', function() {
+    const display = document.querySelector('.numberDisplay');
+    secondNum = display.textContent;
+    result = operate(chosenOperator, firstNum, secondNum);
+    display.textContent = result;
+})
+
+// Enable +/- button
+const posNegButton = document.querySelector('.posNeg');
+posNegButton.addEventListener('click', function() {
+    const display = document.querySelector('.numberDisplay');
+    currentDisplay = display.textContent;
+    display.textContent = currentDisplay * -1;
+    displayNow = display.textContent;
+})
+
+// Store current displayed number and operator when operator is clicked
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(function (i) {
+    i.addEventListener('click', function() {
+        updateState(this);
+    })
+});
+
+function updateState(operator) {
+    const display = document.querySelector('.numberDisplay')
+    if (chosenOperator === "") {
+        firstNum = displayNow;
+        chosenOperator = operator.textContent;
+        display.textContent = "";
+        displayNow = "";
+    } else {
+        secondNum = displayNow;
+        let result = operate(chosenOperator, firstNum, secondNum);
+        display.textContent = result;
+        firstNum = result;
+    }
+
+}
+
